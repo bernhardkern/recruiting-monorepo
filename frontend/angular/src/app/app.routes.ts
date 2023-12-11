bd4e8c26cd09352060ff45849ecd1e2833624095
Andi Schröder <andreas.schroeder@iits-consulting.de>
2023-12-11 13:06:16 +0100

    add user components

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {UsersComponent} from "./users/users.component";
 import {UserGridComponent} from "./users/user-grid/user-grid.component";
+import {UserDetailsComponent} from "./users/user-details/user-details.component";
+import {UserFormComponent} from "./users/user-form/user-form.component";
 
 export const routes: Routes = [
   {
@@ -10,7 +12,20 @@ export const routes: Routes = [
       {
         path: 'overview',
         component: UserGridComponent
-      }
+      },
+      {
+        path: ':id/edit',
+        component: UserFormComponent
+      },
+      {
+        path: 'new',
+        component: UserFormComponent
+      },
+      {
+        path: ':id',
+        component: UserDetailsComponent
+      },
+
     ]
   }
 ];
