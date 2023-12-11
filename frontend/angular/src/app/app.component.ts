3901e19a7a387ff1e9bc481f3fd2c5931ee9f7d5
Tamara Doll <tamara.doll@iits-consulting.de>
2023-12-11 13:57:09 +0100

    added layout

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterLink,
+    MatListModule,
+    RouterModule,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular';
+
+  navList = [
+    {
+      title: 'Users',
+      routerLink: '/users/overview',
+    },
+    {
+      title: 'Matches',
+      routerLink: '/matches/overview',
+    },
+    {
+      title: 'Ranking',
+      routerLink: '/ranking',
+    },
+  ];
 }
