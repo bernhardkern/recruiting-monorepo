f479ebe47ab03966bb1e01e9f276d289978465b2
Tamara Doll <tamara.doll@iits-consulting.de>
2023-12-11 17:06:32 +0100

    added footer

---
+++ frontend/angular/src/app/_shared/footer/footer.component.ts
@@ -0,0 +1,52 @@
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, NgIf, RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {
+  @Input() isGridComponent = false;
+  @Input() isEditForm = false;
+  @Input() saveDisabled = false;
+  @Input() save?: Observable<any>;
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {}
+
+  onCancelClicked() {
+    this.navigateBack();
+  }
+
+  onSaveClicked() {
+    this.emitSave();
+  }
+
+  private navigateBack(): void {
+    let navigateCommand: string[];
+    debugger;
+
+    if (this.isEditForm && !this.isGridComponent) {
+      navigateCommand = ['../../'];
+    } else {
+      navigateCommand = ['../'];
+    }
+
+    this.router.navigate(navigateCommand, {
+      relativeTo: this.route,
+    });
+  }
+
+  private emitSave() {
+    // navigate to edit page
+    this.save?.subscribe();
+  }
+}
