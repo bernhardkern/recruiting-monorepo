05b070ff4009619e5ad12c1b7163fa21bee5ef4b
Robert Wloch <robert.wloch@iits-consulting.de>
2024-05-23 17:00:10 +0200

    vue: ignore mockserver and openapi folders when linting

--- frontend/vue/.eslintrc.cjs
+++ frontend/vue/.eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  ignorePatterns: ["api-mock/*", "openapi/*"],
   parserOptions: {
     ecmaVersion: 'latest'
   }
